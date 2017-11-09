06711e675e56693c12e6a3f26cf943b21d664a3d
Dandelirion <33488388+Dandelirion@users.noreply.github.com>
2017-11-10 01:56:51 +0500

    Transitional done

--- TASK2(from MC Donya).sql
+++ TASK2(from MC Donya).sql
@@ -7,6 +7,8 @@ IF EXISTS (
 		WHERE name = N'Voronin'
 )
 DROP DATABASE Voronin
+GO
+
 CREATE DATABASE Voronin
 GO
 
@@ -37,7 +39,21 @@ BEGIN
 END;
 GO
 
-IF OBJECT_ID (N'Regions', N'U') IS NOT NULL
+IF object_id(N'cars.IsValidAutoRegion', N'FN') IS NOT NULL
+    DROP FUNCTION dbo.IsValidAutoRegion
+GO
+
+CREATE FUNCTION cars.IsValidAutoRegion(@autoRegion smallint)  
+RETURNS BIT
+BEGIN
+	declare @firstSymbol smallint = cast(@autoRegion/100 as int);
+	if @autoRegion >= 100 and @firstSymbol != 1 and @firstSymbol != 2 and @firstSymbol != 7 or @autoRegion = 0
+		return 0;
+	return 1;
+END;
+GO
+
+IF OBJECT_ID (N'cars.Regions', N'U') IS NOT NULL
 	DROP TABLE cars.Regions
 GO
 
@@ -46,7 +62,7 @@ CREATE TABLE cars.Regions
 	RegionName nvarchar(50) NOT NULL)
 GO
 
-IF OBJECT_ID (N'RegionNumbers', N'U') IS NOT NULL
+IF OBJECT_ID (N'cars.RegionNumbers', N'U') IS NOT NULL
 	DROP TABLE cars.RegionNumbers
 GO
 
@@ -55,7 +71,7 @@ CREATE TABLE cars.RegionNumbers
 	ConstitutionNum int FOREIGN KEY REFERENCES cars.Regions(ConstitutionNum) NOT NULL)
 GO
 
-IF OBJECT_ID (N'Posts', N'U') IS NOT NULL
+IF OBJECT_ID (N'cars.Posts', N'U') IS NOT NULL
 	DROP TABLE cars.Posts
 GO
 
@@ -64,7 +80,7 @@ CREATE TABLE cars.Posts
 	PostName nvarchar(50) NOT NULL)
 GO
 
-IF OBJECT_ID (N'Directions', N'U') IS NOT NULL
+IF OBJECT_ID (N'cars.Directions', N'U') IS NOT NULL
 	DROP TABLE cars.Directions
 GO
 
@@ -73,41 +89,64 @@ CREATE TABLE cars.Directions
 	DirectionName nvarchar(50) NOT NULL)
 GO
 
-IF OBJECT_ID (N'AutoTypes', N'U') IS NOT NULL
-	DROP TABLE cars.AutoTypes
-GO
-
---CREATE TABLE cars.AutoTypes
---	(TypeID int PRIMARY KEY NOT NULL,
---	TypeName nvarchar(50) NOT NULL)
---GO
-
-IF OBJECT_ID (N'Autos', N'U') IS NOT NULL
+IF OBJECT_ID (N'cars.Autos', N'U') IS NOT NULL
 	DROP TABLE cars.Autos
 GO
 
 CREATE TABLE cars.Autos
 	(AutoID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	AutoNumber nvarchar(50) CHECK(cars.IsValidAutoNum(AutoNumber) = 1) NOT NULL,
-	RegionNumber int FOREIGN KEY REFERENCES cars.RegionNumbers(AutoRegionNumber) NOT NULL,
-	Direction int FOREIGN KEY REFERENCES cars.Directions(DirectionID) NOT NULL)
-	--AutoType int FOREIGN KEY REFERENCES cars.AutoTypes(TypeID) NOT NULL)
+	RegionNumber int CHECK(cars.IsValidAutoRegion(RegionNumber) = 1)
+	    FOREIGN KEY REFERENCES cars.RegionNumbers(AutoRegionNumber) NOT NULL)
 GO
 
-IF OBJECT_ID (N'RegistrationRecords', N'U') IS NOT NULL
+IF OBJECT_ID (N'cars.RegistrationRecords', N'U') IS NOT NULL
 	DROP TABLE cars.RegistrationRecords
 GO
 
 CREATE TABLE cars.RegistrationRecords
-	(RecordID int IDENTITY(1,1) NOT NULL,
+	(RecordID int PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	PostID int FOREIGN KEY REFERENCES cars.Posts(PostID) NOT NULL,
 	AutoID int FOREIGN KEY REFERENCES cars.Autos(AutoID) NOT NULL,
-	RecordTime datetime NOT NULL)
+	Direction int FOREIGN KEY REFERENCES cars.Directions(DirectionID) NOT NULL,
+	RecordTime time(0) NOT NULL)
+GO
+
+CREATE TRIGGER CheckDirection
+ON cars.RegistrationRecords
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @insertedRecordID int
+	DECLARE @insertedAutoID int
+	DECLARE @insertedDirection int
+	SET @insertedRecordID = (SELECT TOP 1 RecordID
+						   FROM inserted
+						   ORDER BY RecordID DESC)
+	SET @insertedAutoID = (SELECT TOP 1 AutoID
+						   FROM inserted
+						   ORDER BY RecordID DESC)
+	SET @insertedDirection = (SELECT TOP 1 Direction
+							  FROM inserted
+							  ORDER BY RecordID DESC)
+	DECLARE @lastCapturedAutoDirection int
+	SET @lastCapturedAutoDirection = (SELECT TOP 1 Direction
+								  FROM cars.RegistrationRecords
+								  WHERE cars.RegistrationRecords.AutoID = @insertedAutoID and
+								   cars.RegistrationRecords.RecordID < @insertedRecordID
+								  ORDER BY RecordID DESC)
+	IF @insertedDirection = @lastCapturedAutoDirection
+	BEGIN
+		PRINT 'Автомобиль не может несколько раз подряд въехать/выехать'
+		ROLLBACK TRANSACTION
+	END
+END
 GO
 
 INSERT INTO cars.Regions values (66, 'Свердловская область')
 INSERT INTO cars.Regions values (74, 'Челябинская область')
 INSERT INTO cars.Regions values (59, 'Пермский край')
+GO
 
 INSERT INTO cars.RegionNumbers values (66, 66)
 INSERT INTO cars.RegionNumbers values (96, 66)
@@ -117,14 +156,76 @@ INSERT INTO cars.RegionNumbers values (174, 74)
 INSERT INTO cars.RegionNumbers values (59, 59)
 INSERT INTO cars.RegionNumbers values (81, 59)
 INSERT INTO cars.RegionNumbers values (159, 59)
+GO
 
 INSERT INTO cars.Posts values ('Север')
 INSERT INTO cars.Posts values ('Юг')
 INSERT INTO cars.Posts values ('Запад')
 INSERT INTO cars.Posts values ('Восток')
+GO
 
 INSERT INTO cars.Directions values('в город')
 INSERT INTO cars.Directions values('из города')
+GO
 
-INSERT INTO cars.Autos values('В123АН', 196, 1)
-INSERT INTO cars.Autos values('Ё123АН', 196, 1)
+INSERT INTO cars.Autos values('В123АН', 196)
+INSERT INTO cars.Autos values('В124АН', 74)
+--INSERT INTO cars.Autos values('Ё123АН', 196, 1) проверка check
+GO
+
+INSERT INTO cars.RegistrationRecords values(1, 2, 1, '14:15:16')
+INSERT INTO cars.RegistrationRecords values(2, 2, 2, '14:30:16')
+--INSERT INTO cars.RegistrationRecords values(1, 2, 1, '14:15:17')
+--INSERT INTO cars.RegistrationRecords values(1, 1, 2, '14:15:18')
+--INSERT INTO cars.RegistrationRecords values(1, 1, 1, '14:15:18') проверка триггера
+--TRUNCATE TABLE cars.RegistrationRecords
+GO
+
+
+----------два вспомогательных представления для вычисления типов авто-------------
+IF OBJECT_ID (N'cars.FirstAutosRegistration', N'U') IS NOT NULL
+	DROP VIEW cars.FirstAutosRegistration
+GO
+
+CREATE VIEW cars.FirstAutosRegistration
+AS
+SELECT AutoID, PostID, Direction, MIN(RecordTime) AS firstTime
+FROM cars.RegistrationRecords
+GROUP BY AutoID, PostID, Direction
+GO
+
+IF OBJECT_ID (N'cars.LastAutosRegistration', N'U') IS NOT NULL
+	DROP VIEW cars.LastAutosRegistration
+GO
+
+CREATE VIEW cars.LastAutosRegistration
+AS
+SELECT AutoID, PostID, Direction, MAX(RecordTime) AS lastTime
+FROM cars.RegistrationRecords
+GROUP BY AutoID, PostID, Direction
+GO
+-----------------------------------------------------------------------------------
+
+----------Типы автомобилей---------------------------------------------------------
+IF OBJECT_ID (N'cars.TransitionalAutos', N'U') IS NOT NULL
+	DROP VIEW cars.TransitionalAutos
+GO
+
+CREATE VIEW cars.TransitionalAutos
+AS
+SELECT DISTINCT cars.Autos.AutoNumber AS 'Номер'
+				, cars.Autos.RegionNumber AS 'Номер региона'
+				, cars.Regions.RegionName AS 'Название региона'
+				, far.firstTime AS 'Время первого въезда'
+				, lar.lastTime AS 'Время последнего выезда'
+FROM cars.Autos INNER JOIN
+	 cars.RegionNumbers ON cars.Autos.RegionNumber = cars.RegionNumbers.AutoRegionNumber INNER JOIN
+	 cars.Regions ON cars.RegionNumbers.ConstitutionNum = cars.Regions.ConstitutionNum INNER JOIN
+	 cars.FirstAutosRegistration AS far ON cars.Autos.AutoID = far.AutoID INNER JOIN
+	 cars.LastAutosRegistration AS lar ON cars.Autos.AutoID = lar.AutoID
+WHERE firstTime < lastTime 
+	  AND far.Direction = 1 
+	  AND lar.Direction = 2
+	  AND far.PostID != lar.PostID
+	  AND cars.RegionNumbers.ConstitutionNum != 66
+GO
